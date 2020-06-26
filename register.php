6f0833e34815175e251e87ec6715247d87a63b05
mridulasm <serahmridula@gmail.com>
2020-06-26 21:02:38 +0530

    minor fix

--- register.php
+++ register.php
@@ -5,14 +5,36 @@
     $p_age = $_POST['p_age'];
     
     // Create connection
-    $conn = mysqli_connect("localhost", "root","", "myDataBase3");
+    $conn = mysqli_connect("localhost", "root","", "DataBase1");
     // Check connection
-    if (!$conn) 
-      die("Connection failed: " . mysqli_connect_error());
-    else
-        echo "<br>Connection successful";
+      if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+      
+      // Create database
+      $sql = "CREATE DATABASE DataBase1";
+      if (mysqli_query($conn, $sql)) 
+      {
+        echo "Database created successfully";
+      } else {
+        echo "Error creating database: " . mysqli_error($conn);
+      }
+      // sql to create table
+    $sql = "CREATE TABLE Patients (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+    if (mysqli_query($conn, $sql)) {
+      echo "Table Patients created successfully";
+    } else {
+      echo "Error creating table: " . mysqli_error($conn);
+    }
     
-    $insert = "Inert into patients_details (p_name,p_address,p_no,p_age)
+    $insert = "Inert into Patients (p_name,p_address,p_no,p_age)
     values ('$p_name', '$p_address', '$p_no', '$p_age')";
     
     if ($conn->query($insert)) 
